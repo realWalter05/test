2b62300df2b92e1ddda0b7a2713cb0ed210196eb
Walter <zikavaclav05@gmail.com>
2021-02-08 23:45:38 +0100

    everything

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+s = requests.session()
+
+
+def send_payload(from_url, to_url, data_payload):
+    s.post(from_url, data=data_payload)
+    page = s.get(to_url)
+
+    if not page.ok:
+        print(f"Payload {from_url} to {to_url} error! Status code of page: {page.status_code}")
+    return page
 
 
 @app.route('/')
 def index():
+    payload = {
+        "username": "zikav29z",
+        "password": "1c2zkH51",
+        "returnUrl": "/dashboard",
+        "login": "",
+    }
+    page_komens = send_payload("https://zsebenese.bakalari.cz/Login", "https://zsebenese.bakalari.cz/next/komens.aspx?s=rok",
+                               payload)
     return "<h1>Welcome to our server !!</h1>"
